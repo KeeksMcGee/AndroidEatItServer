42922e4cece142fb9fb56166a78381e4a72a1120
KeeksMcGee <kiarra.gibbs@gmail.com>
2019-11-24 19:34:56 -0500

    Final Project

--- app/src/main/java/com/example/androideatitserver/Home.java
+++ app/src/main/java/com/example/androideatitserver/Home.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.androideatitserver.Common.Common;
+import com.example.androideatitserver.Interface.ItemClickListener;
 import com.example.androideatitserver.Model.Category;
 import com.example.androideatitserver.ViewHolder.MenuViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -77,7 +78,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     Category newCategory;
 
     Uri saveUri;
-    private final int PICK_IMAGE_REQUEST = 71;
+    private final int PICK_IMAGE_REQUEST = 1;
     DrawerLayout drawer;
 
     @Override
@@ -203,7 +204,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                                 @Override
                                 public void onSuccess(Uri uri) {
                                     //set value for newCategory if image uploads and we can get download link
-                                    newCategory = new Category(edtName.getText().toString(),uri.toString());
+                                    newCategory = new Category((edtName.getText().toString()),uri.toString());
                                 }
                             });
                         }
@@ -255,10 +256,18 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 categories
         ) {
             @Override
-            protected void populateViewHolder(MenuViewHolder menuViewHolder, Category category, int i) {
+            protected void populateViewHolder(MenuViewHolder menuViewHolder, Category category, int position) {
                 menuViewHolder.txtMenuName.setText(category.getName());
                 Picasso.with(Home.this).load(category.getImage())
                 .into(menuViewHolder.imageView);
+
+                menuViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        //Code later
+                    }
+                });
+
             }
         };
         adapter.notifyDataSetChanged();//Refresh
@@ -305,4 +314,126 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         return true;
     }
 
+    //Update /Delete
+    //Press Ctrl+O
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getTitle().equals(Common.UPDATE))
+        {
+            showUpdateDialog(adapter.getRef(item.getOrder()).getKey(),adapter.getItem(item.getOrder()));
+        }
+        else  if(item.getTitle().equals(Common.DELETE))
+        {
+            deleteCategory(adapter.getRef(item.getOrder()).getKey());
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteCategory(String key) {
+        categories.child(key).removeValue();
+        Toast.makeText(this, "Item deleted!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showUpdateDialog(final String key, final Category item) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Update Category");
+        alertDialog.setMessage("Please fill out information fully");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_menu_layout,null);
+
+        edtName = add_menu_layout.findViewById(R.id.edtName);
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+
+        //Set default name
+        edtName.setText(item.getName());
+
+        //Event for button
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage(); //Let user select image from Gallery and save URL of this image
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeImage(item);
+            }
+        });
+
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        //Set button
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+
+                //Update information
+                item.setName(edtName.getText().toString());
+                categories.child(key).setValue(item);
+
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void changeImage(final Category item) {
+        if(saveUri !=null)
+        {
+            final ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageReference.child("images/"+imageName);
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this,"Uploaded!",Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    //set value for newCategory if image uploads and we can get download link
+                                    item.setImage(uri.toString());
+                                }
+                            });
+                        }
+
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            //Don't worry about these errors
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded "+progress+"%");
+                        }
+                    });
+
+        }
+    }
 }
--- app/src/main/java/com/example/androideatitserver/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/androideatitserver/ViewHolder/MenuViewHolder.java
@@ -1,17 +1,22 @@
 package com.example.androideatitserver.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androideatitserver.Common.Common;
 import com.example.androideatitserver.Interface.ItemClickListener;
 import com.example.androideatitserver.R;
 
 
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements
+        View.OnClickListener,
+        View.OnCreateContextMenuListener
+{
 
     public TextView txtMenuName;
     public ImageView imageView;
@@ -24,8 +29,8 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
         txtMenuName = (TextView)itemView.findViewById(R.id.menu_name);
         imageView = (ImageView)itemView.findViewById(R.id.menu_image);
 
+        itemView.setOnCreateContextMenuListener(this);
         itemView.setOnClickListener(this);
-
     }
 
     public void setItemClickListener(ItemClickListener itemClickListener) {
@@ -35,5 +40,13 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View view){
         itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+        contextMenu.setHeaderTitle("Select the action");
+
+        contextMenu.add(0, 0, getAdapterPosition(), Common.UPDATE);
+        contextMenu.add(0, 1, getAdapterPosition(), Common.DELETE);
     }
 }
